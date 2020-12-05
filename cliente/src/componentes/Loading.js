fa0afa988c8bd1ab5ff0e6752ef900a73b09906b
Joaquin <sol19008@byui.edu>
2020-12-05 17:07:55 -0300

    upload loading component

---
+++ cliente/src/componentes/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Loading() {
+    return (
+        <div className="Loading">
+            <div className="Loaging__dot-1"></div>
+            <div className="Loaging__dot-2"></div>
+            <div className="Loaging__dot-3"></div>
+        </div>
+    )
+}
